0d841ca876a7991f34d915a0d9aa64e369b94532
Hugo MOCHET <mochet.hugo@gmail.com>
2017-05-16 12:44:19 +0200

    Fix article test
    When the content of an article is empty it raise a LangDetectException(ErrorCode.CantDetectError, 'No features in text.')

--- blog/test.py
+++ blog/test.py
@@ -5,7 +5,8 @@ from .models import Article
 
 class ArticleTest(TestCase):
     def test_title_clean_html(self):
-        article = Article(title='<h1>title<br></h1>', content='').save()
+        article = Article(title='<h1>title<br></h1>',
+                          content='<p>content</p>').save()
         self.assertEqual(article.title, 'title')
 
     def test_content_cleaned_html(self):
