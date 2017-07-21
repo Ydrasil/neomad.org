c45c2962befdcf38b7f0dbce0dff86868ba3893e
fuentesloic <fuentes.loic@gmail.com>
2017-07-21 08:52:27 -0400

    search template

--- search/views.py
+++ search/views.py
@@ -13,5 +13,5 @@ def search():
     regex = re.compile('.*{}.*'.format(request.args.get('q')))
     users = User.objects(slug=regex)[:10]
     articles = Article.objects(content=regex)[:10]
-    return render_template('search/search.html', users=users,
+    return render_template('search/list.html', users=users,
                            articles=articles)
